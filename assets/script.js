17d7c059c1935b9a820b504f6e13dfe480bea489
mattkohl82 <mattkohl82@gmail.com>
2020-05-09 15:03:28 -0500

    it works

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,59 @@
 function writePassword() {
-  var clickButton = window.prompt('Would you like to make a password?');
-  if (clickButton === "" || clickButton === null) {
-    window.alert("You need to provide an answer! Please try again.");
+  var clickButton = window.confirm('Would you like to make a password?')
+  if (!clickButton) {
+    window.alert("You need to provide an answer! Please try again.")
   return writePassword();
-  }
+  } 
 
-  clickButton = clickButton.toLowerCase();
-
-  if (clickButton === "no") {
-    var noThanks = window.confirm("Are you sure?");
-
-    if (noThanks) {
-      window.alert("OK goodbye");
+  const passLength = window.prompt('How long between 8-124 do you want your password?')
+    if (!passLength  || parseInt(passLength) < 8 || parseInt(passLength) > 124) {
+      window.alert("please enter value between 8 - 124")
       return writePassword();
     }
+  const lowerConfirm = window.confirm('Would you like to use Lowercase letter?')
+  const upperConfirm = window.confirm('Would you like to use Uppercase letters?')
+  const numConfirm = window.confirm('Would you like to use numbers?')
+  const symConfirm = window.confirm('Would you like to use symbols?')
+
+  if (!lowerConfirm && upperConfirm === false && numConfirm === false && symConfirm === false) {
+    window.alert('You didnt select any items for your password, Try again.')
+    return writePassword();
   }
 
-  if (clickButton === "yes") {
-    var yesPassword = window.confirm("Ok! Your password will be completely random and between 8 - 124 characters!");
-  }
-  var password = generatePassword();
+
+  var password = generatePassword(lowerConfirm, upperConfirm, numConfirm, symConfirm, parseInt(passLength));
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
-function generatePassword() {
+function generatePassword(lowerConfirm, upperConfirm, numConfirm, symConfirm, passLength) {
+  
+  let passArray = []
 
-  var min = 8;
-  var max = 124;
+  const lowerArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-  let passArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','O','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0','!','#','@','$','%','^','&','*','(',')','?','>','<'];
-  var length = Math.floor((Math.random() * max) + min);
+  const upperArray = ['A','B','C','D','E','F','G','H','I','J','K','L','O','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+  
+  const numArray = ['1','2','3','4','5','6','7','8','9','0']
+
+  const symArray = ['!','#','@','$','%','^','&','*','(',')','?','>','<']
+
+  if (lowerConfirm === true) {
+    passArray = [...lowerArray]
+  } 
+  if (upperConfirm === true) {
+    passArray = [...upperArray, ...passArray]
+  }
+  if (numConfirm === true) {
+    passArray = [...numArray, ...passArray]
+  }
+  if (symConfirm ===true) {
+    passArray = [...symArray, ...passArray]
+  }
 
   let password = "";
-  for(i = 0; i <= length; i++) {
+  for(i = 1; i <= passLength; i++) {
     let randomNum = Math.floor(Math.random() * passArray.length);
     password += passArray[randomNum];
   }
